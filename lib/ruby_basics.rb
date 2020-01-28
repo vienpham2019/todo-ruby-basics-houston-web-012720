69abbee3d23e19ed68bc7be82946e6a10209bc11
vien pham <vienpham2019@gmail.com>
2020-01-28 18:33:05 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,17 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
 def argue
-  
+  puts "I'm right you are wrong"
 end
 
-def greeting
-
+def greeting(greeting,name)
+  return "#{greeting}#{name}"
 end
 
 def return_a_value
